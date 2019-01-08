399c23921f38d946748823c684acfca2a997abb4
Andrey Oshev <faca.system@gmail.com>
2019-01-09 00:48:26 +0500

    light only

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
 
-    homebridge.registerAccessory('homebridge-mi-gateway', 'MiGateway', MiGateway);
+    homebridge.registerAccessory('homebridge-mi-gateway-light', 'MiGateway', MiGateway);
 }
 
 function isConfig(configFile, type, name) {
@@ -79,27 +79,6 @@ MiGateway.prototype = {
             .on('get', this.getSaturation.bind(this))
             .on('set', this.setSaturation.bind(this));
         services.push(lightService);
-
-        var fmService = new Service.Switch(this.config.fmName || "FM");
-        fmService
-            .getCharacteristic(Characteristic.On)
-            .on('get', this.getFMStatus.bind(this))
-            .on('set', this.setFMStatus.bind(this));
-        services.push(fmService);
-
-        var securityService = new Service.SecuritySystem(this.config['securityName'] || "Security");
-        securityService
-            .getCharacteristic(Characteristic.SecuritySystemCurrentState)
-            .setProps({ validValues: [1, 3]})
-            .on('get', this.getCurrentSecurity.bind(this))
-            .value = Characteristic.SecuritySystemCurrentState.DISARMED;
-        securityService
-            .getCharacteristic(Characteristic.SecuritySystemTargetState)
-            .setProps({ validValues: [1, 3]})
-            .on('get', this.getTargetSecurity.bind(this))
-            .on('set', this.setTargetSecurity.bind(this))
-            .value = Characteristic.SecuritySystemCurrentState.DISARM;
-        services.push(securityService);
 
         return services;
     },
@@ -210,103 +189,4 @@ MiGateway.prototype = {
             }
         }).catch(callback);
     },
-
-    /////////////////////////////////////////////////////////////////////
-    // FM
-    /////////////////////////////////////////////////////////////////////
-
-    getFMStatus: function (callback) {
-        var that = this;
-        this.getDevice().then(res => {
-            return res.device.call("get_prop_fm", []);
-        }).then(result => {
-            callback(null, result['current_status'] === 'pause' ? false : true);
-        }).catch(function(err) {
-            that.log.error("[MiGateway][ERROR]getFMStatus Error: " + err);
-            callback(err);
-        });
-    },
-
-    setFMStatus: function (value, callback) {
-        var that = this;
-        this.getDevice().then(res => {
-            return res.device.call("play_fm", [ value ? "on" : "off"]);
-        }).then(result => {
-            if(result[0] === "ok") {
-                callback(null);
-            } else {
-                callback(new Error(result[0]));
-            }
-        }).catch(function(err) {
-            that.log.error("[MiGateway][ERROR]setFMStatus Error: " + err);
-            callback(err);
-        });
-    },
-
-    /////////////////////////////////////////////////////////////////////
-    // Security
-    /////////////////////////////////////////////////////////////////////
-
-    getCurrentSecurity: function (callback) {
-        var that = this;
-        this.getDevice().then(res => {
-            return res.device.call("get_arming", []);
-        }).then(result => {
-            var value = null;
-            if(result[0] === 'on') {
-                callback(null, Characteristic.SecuritySystemCurrentState.AWAY_ARM);
-            } else if(result[0] === 'off') {
-                callback(null, Characteristic.SecuritySystemCurrentState.DISARMED);
-            } else {
-                callback(new Error(result[0]));
-            }
-        }).catch(function(err) {
-            that.log.error("[MiGateway][ERROR]get Current Security Error: " + err);
-            callback(err);
-        });
-    },
-
-    getTargetSecurity: function (callback) {
-        var that = this;
-        this.getDevice().then(res => {
-            return res.device.call("get_arming", []);
-        }).then(result => {
-            var value = null;
-            if(result[0] === 'on') {
-                callback(null, Characteristic.SecuritySystemTargetState.AWAY_ARM);
-            } else if(result[0] === 'off') {
-                callback(null, Characteristic.SecuritySystemTargetState.DISARM);
-            } else {
-                callback(new Error(result[0]));
-            }
-        }).catch(function(err) {
-            that.log.error("[MiGateway][ERROR]get Target Security Error: " + err);
-            callback(err);
-        });
-    },
-
-    setTargetSecurity: function (value, callback) {
-        var val = "off";
-        if (Characteristic.SecuritySystemCurrentState.AWAY_ARM == value) {
-            val = "on";
-        } else if (Characteristic.SecuritySystemCurrentState.DISARMED == value) {
-            val = "off";
-        } else {
-            val = "off";
-        }
-
-        var that = this;
-        this.getDevice().then(res => {
-            return res.device.call("set_arming", [val]);
-        }).then(result => {
-            if(result[0] === "ok") {
-                callback(null);
-            } else {
-                callback(new Error(result[0]));
-            }
-        }).catch(function(err) {
-            that.log.error("[MiGateway][ERROR] set Target Security Error: " + err);
-            callback(err);
-        });
-    }
 }
